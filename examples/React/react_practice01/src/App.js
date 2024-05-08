31958ee8856bfb1d6d9b4b3e14c08eb50bd5c70e
HaYoung Lee <lhy0118@midasit.com>
2024-05-08 16:17:10 +0900

    react_practice01

---
+++ examples/React/react_practice01/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from "react";
+import Todos from './components/Todos'
+
+function App() {
+
+  const [name, setName] = useState("lhy0118");
+
+  useEffect(()=>{
+    console.log('use effect')
+    return ()=>{
+      console.log('cleanup')
+    }
+  }, [name])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p onClick={()=>setName("TGC")}>
+          {name}
+        </p>
+        {/* <Todos/> */}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
