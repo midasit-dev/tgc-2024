6fe45864fa274d36896df504e6591e5dda1f9182
hye0408 <hye0408@midasin.com>
2024-05-13 16:25:07 +0900

    과제_240513

--- projects/hye0408/React/react_practice01/src/components/Todos.js
+++ projects/hye0408/React/react_practice01/src/components/Todos.js
@@ -7,14 +7,18 @@ import { Button } from "@midasit-dev/moaui";
 function Todos() {
   const [counter, setCounter] = useState(1);
   // api 호출을 통해 받아온 데이터를 todos라는 이름으로 사용합니다. (내부에서 useState, useEffect 사용됨)
+  const [count, setCount] = useState(0);
   const todos = useTodos(counter);
+ 
 
   function onClickButton() {
-    setCounter(counter+1);
+    setCounter(counter+1)
+    setCount(count+1);
   }
-
+  
   return (
     <div className="Todos">
+      <p>버튼이 {count} 번 클릭되었습니다.</p>
       <Button
         variant="contained"
         width="auto"
@@ -24,17 +28,29 @@ function Todos() {
         Add Todo list
       </Button>
       <ul>
-        {
-          // todos 배열을 순회하며 각각의 요소를 <li> 태그를 사용하여 출력합니다.
-          // 이렇게 반복되는 요소에 key라는 프로퍼티가 빠지면 콘솔에 경고가 뜹니다.
-          todos.map((todo) => (
+        {todos.map((todo) => (
             <li key={todo.id}>{todo.todo}</li>
-          ))
-        }
+          ))}
       </ul>
+
     </div>
   );
 }
 
+// 새로운 컴포넌트를 정의합니다. -> 숫자세기
+/* function Counter() {
+  const [count, setCount] = useState(0);
+  
+  return (
+    <div className="Counter">
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}*/
+
+
 // 정의한 컴포넌트를 외부에서 사용할 수 있또록 export 합니다.
-export default Todos;
+export default Todos ;
