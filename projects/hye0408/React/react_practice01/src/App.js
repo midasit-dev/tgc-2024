99234728bb05d2f7b39f3624eb75d3daea58a360
hye0408 <hye0408@midasin.com>
2024-05-13 16:25:26 +0900

    과제2_240513

--- projects/hye0408/React/react_practice01/src/App.js
+++ projects/hye0408/React/react_practice01/src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Todos from "./components/Todos";
+import Todos from "./components/Todos.js";
 import { Button } from "@midasit-dev/moaui";
 
 function App() {
   const [name, setName] = React.useState("TGC");
   const [test, setTest] = React.useState("test");
+  const [count, setCount] = React.useState(0);
 
   React.useEffect(() => {
     console.log("click2")
     setTest("test2")
   }, [name]);
+
+  React.useEffect(() => {
+    setCount(`남은 횟수는 ${150-count} 번 입니다`);
+  }, [count]);
 
   function onClickName(event) {
     console.log("click1")
@@ -33,6 +38,7 @@ function App() {
         <div className="div_name" onClick={onClickName}>
           {name}
         </div>
+        <div id="spare_count"> {count} </div>
         <Todos/>
       </header>
     </div>
