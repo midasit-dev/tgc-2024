6e557034927bf4841cd0542cd0d1495fab622928
khd0811 <khd0811@midasit.com>
2024-05-14 16:55:09 +0900

    TGC-8주차 과제 제출
    useState, useEffect 추가

--- projects/khd0811/Todos.js
+++ projects/khd0811/Todos.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Todos.css";
 import useTodos from "../hooks/useTodos";
 import { Button } from "@midasit-dev/moaui";
@@ -8,10 +8,19 @@ function Todos() {
   const [counter, setCounter] = useState(1);
   // api 호출을 통해 받아온 데이터를 todos라는 이름으로 사용합니다. (내부에서 useState, useEffect 사용됨)
   const todos = useTodos(counter);
-   // completed가 true인 todos와 false인 todos의 개수를 계산합니다!
-   const countAll = todos.length;
-   const completedCount_true = todos.filter(todo => todo.completed).length;
-   const completedCount_false = countAll - completedCount_true;
+
+  // 상태로 관리될 카운트 변수들을 선언합니다.
+  const [countAll, setCountAll] = useState(0);
+  const [completedCountTrue, setCompletedCountTrue] = useState(0);
+  const [completedCountFalse, setCompletedCountFalse] = useState(0);
+
+  // todos 배열이 변경될 때마다 카운트를 업데이트 합니다.
+  useEffect(() => {
+    setCountAll(todos.length);
+    const completedTrue = todos.filter(todo => todo.completed).length;
+    setCompletedCountTrue(completedTrue);
+    setCompletedCountFalse(todos.length - completedTrue);
+  }, [todos]);  // todos 배열이 업데이트될 때마다 이 effect를 실행합니다.
 
   function onClickButton() {
     setCounter(counter + 1)
@@ -28,7 +37,7 @@ function Todos() {
         Add Todo list
       </Button>
       <div>
-      Count: {countAll} | Completed: {completedCount_true} | Not Completed: {completedCount_false}
+      Count: {countAll} | Completed: {completedCountTrue} | Not Completed: {completedCountFalse}
       </div>
       <ul>
         {
@@ -43,5 +52,5 @@ function Todos() {
   );
 }
 
-// 정의한 컴포넌트를 외부에서 사용할 수 있또록 export 합니다.
+// 정의한 컴포넌트를 외부에서 사용할 수 있도록 export 합니다.
 export default Todos;
