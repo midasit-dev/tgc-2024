a9a294fd464c497953f7ba52c97c1adfde6f3e1b
lhy0118 <lhy0118@midasit.com>
2024-05-14 10:42:59 +0900

    practice02 UI 개발 완료

--- examples/React/react_practice02/src/App.tsx
+++ examples/React/react_practice02/src/App.tsx
@@ -12,69 +12,148 @@
  */
 
 import React from "react";
-import { GuideBox, Panel, DataGrid, Button, DataGridSample } from "@midasit-dev/moaui";
+import { GuideBox, Panel, DataGrid, Button } from "@midasit-dev/moaui";
 import PreviewShape from "./Component/PreviewShape";
+import MDReport from "./Component/MarkdownView";
 
 const initRows = [
-	{ id: 1, x: 0, y: 0 },
-	{ id: 2, x: 1, y: 1 },
-	{ id: 3, x: 2, y: 0 }
+  { id: 1, x: 10, y: 10 },
+  { id: 2, x: 300, y: 10 },
+  { id: 3, x: 300, y: 300 },
+  { id: 4, x: 10, y: 300 },
 ];
 
-const trianglePoints = [{ x: 100, y: 100 }, { x: 150, y: 150 }, { x: 100, y: 200 }];
+const totaldataSet = {
+  cord: [],
+};
 
 const App = () => {
-	const [rowDatas, setRowDatas] = React.useState(initRows);
+  const [rowDatas, setRowDatas] = React.useState(initRows);
+
+  function onClickCalculate() {
+    //convert rowDatas to totaldataSet
+    const cord = {
+      ...totaldataSet,
+      cord: rowDatas.map((row) => [row.x, row.y]),
+    };
+    console.log("cord", cord); // cord 변수를 python에 넘겨줄 예정.
+  }
+
+  function onClickAdd() {
+    const newId = rowDatas[rowDatas.length - 1].id + 1;
+    setRowDatas([...rowDatas, { id: newId, x: 0, y: 0 }]);
+  }
+
+  function onClickDelete() {
+    if (rowDatas.length === 0) return;
+    const lastId = rowDatas[rowDatas.length - 1].id;
+    setRowDatas(rowDatas.filter((row) => row.id !== lastId));
+  }
 
   return (
     <GuideBox width={650} height={700} spacing={2} padding={2} row>
       <GuideBox>
-        <Panel title="Welcome to MoaUI" width={300} height={300} justifyContent={"center"} display={"flex"} alignItems={"center"}>
-          <PreviewShape Rowdata={rowDatas}/>
+        <Panel
+          title="Welcome to MoaUI"
+          width={300}
+          height={300}
+          justifyContent={"center"}
+          display={"flex"}
+          alignItems={"center"}
+          padding={0}
+        >
+          <PreviewShape rowdata={rowDatas} />
         </Panel>
-				<Panel marginTop={2} marginBottom={2} width={300} height={300}>
-          <DataGrid
-            checkboxSelection
-            columns={[
-              {
-                editable: false,
-                field: "id",
-                headerName: "ID",
-                width: 50,
-              },
-              {
-                editable: true,
-                field: "x",
-                headerName: "X",
-                width: 60,
-              },
-              {
-                field: "y",
-                headerName: "Y",
-                width: 60,
-              }
-            ]}
-            initialState={{
-              pagination: {
-                paginationModel: {
-                  pageSize: 5,
+        <Panel marginTop={2} marginBottom={2} width={300} height={300}>
+          {/* <TestView /> */}
+          <GuideBox
+            height={240}
+            width={280}
+            marginBottom={1}
+            overflow="hidden"
+            center
+          >
+            <DataGrid
+              columns={[
+                {
+                  editable: false,
+                  field: "id",
+                  headerName: "ID",
+                  width: 40,
                 },
-              },
-            }}
-            pageSizeOptions={[5]}
-            rows={rowDatas}
-          />
+                {
+                  editable: true,
+                  field: "x",
+                  headerName: "X",
+                  width: 110,
+                },
+                {
+                  editable: true,
+                  field: "y",
+                  headerName: "Y",
+                  width: 110,
+                },
+              ]}
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 5,
+                  },
+                },
+              }}
+              pageSizeOptions={[5]}
+              rows={rowDatas}
+              hideFooter
+              hideFooterPagination
+              hideFooterSelectedRowCount
+              processRowUpdate={(newValue: any) => {
+                setRowDatas(
+                  rowDatas.map((row) =>
+                    row.id === newValue.id
+                      ? {
+                          id: newValue["id"],
+                          x: Number(newValue["x"]),
+                          y: Number(newValue["y"]),
+                        }
+                      : row
+                  )
+                );
+                return newValue;
+              }}
+              onProcessRowUpdateError={(error: any) => {
+                console.log("error", error);
+              }}
+            />
+          </GuideBox>
+          <GuideBox row horRight spacing={1} marginRight={1}>
+            <Button width="70px" color="normal" onClick={onClickAdd}>
+              Add
+            </Button>
+            <Button width="70px" color="negative" onClick={onClickDelete}>
+              Delete
+            </Button>
+          </GuideBox>
         </Panel>
-				<Button onClick={() => {}} variant="contained" width="100%">
-					Calculate
-				</Button>
-
+        <Button onClick={onClickCalculate} variant="contained" width="100%">
+          Calculate
+        </Button>
       </GuideBox>
-			<GuideBox>
-				<Panel width={300} height={660}>
-					계산서
-				</Panel>
-			</GuideBox>
+      <GuideBox>
+        <Panel width={300} height={660} padding={1} >
+					<GuideBox center>
+						<div
+							style={{
+								height: 640,
+								width: 280,
+								overflowY: "scroll",
+								fontSize: 12,
+							}}
+						>
+							<MDReport />
+						</div>
+					</GuideBox>
+        </Panel>
+      </GuideBox>
     </GuideBox>
   );
 };
--- examples/React/react_practice02/src/Component/PreviewShape.jsx
+++ examples/React/react_practice02/src/Component/PreviewShape.jsx
@@ -1,59 +1,93 @@
-import React from 'react';
-import { Stage, Layer, Line, Rect, Text  } from 'react-konva';
+import React, { useState, useEffect } from "react";
 
+// Point to string 변환 (예: "10,10 290,150 10,290")
 
-const shapeCSS = {
-  fill: 'rgba(0, 0, 0, 0.5)',
-  stroke: 'black',
-  strokeWidth: 1,
-};
+const Graph = ({ points = [] }) => {
+  const [scaledPoints, setScaledPoints] = useState([""]);
 
-export default function PreviewShape({ Rowdata }) {
-  // 각 좌표를 평탄화하고 Konva의 도형에 맞는 형식으로 변환
-  const flattenedPoints = Rowdata.reduce((acc, { x, y }) => [...acc, x*100, y*100], []);
-
-  const drawShape = () => {
-    switch (Rowdata.length) {
-      case 3:
-        // 삼각형
-        return (
-          <>
-            <Line points={flattenedPoints} {...shapeCSS} closed />
-            {Rowdata.map((row) => (
-              <Text
-                key={row.id}
-                x={row.x * 100}
-                y={row.y * 100}
-                text={String(row.id)}
-                fontSize={10}
-                fontFamily='Pretendard'
-                fill='black'
-                offsetX={10} // 텍스트 위치 조정
-                offsetY={5}
-              />
-            ))}
-            </>
-        );
-      case 4:
-        // 사각형
-        // 사각형의 경우, x, y를 시작점으로 하고 넓이와 높이를 계산해야 하므로 별도의 처리가 필요
-        const [x1, y1, x2, y2, x3, y3] = flattenedPoints;
-        const rectWidth = Math.abs(x3 - x1);
-        const rectHeight = Math.abs(y2 - y1);
-        return <Rect x={x1} y={y1} width={rectWidth} height={rectHeight} {...shapeCSS} />;
-      case 5:
-        // 오각형
-        return <Line points={flattenedPoints} {...shapeCSS} closed />;
-      default:
-        return null;
-    }
+  const pointsToString = (pointsValue) => {
+    if (!pointsValue || pointsValue.length === 0) return "";
+    return pointsValue
+      .map((pointValue) => `${pointValue.x},${pointValue.y}`)
+      .join(" ");
   };
 
+  const scale = (point, points) => {
+    // 모든 점 중에서 최대 x 및 y 값을 찾습니다.
+    const maxX = Math.max(...points.map((p) => p.x));
+    const maxY = Math.max(...points.map((p) => p.y));
+
+    // scaleX과 scaleY를 계산합니다.
+    const scaleX = 290 / maxX;
+    const scaleY = 290 / maxY;
+
+    // 축소해야 하는 경우 작은 비율을 선택하여 모든 점이 300x300 범위 내에 있도록 합니다.
+    const scale = Math.min(scaleX, scaleY, 1); // 1은 이미 충분히 작은 경우를 처리하기 위함입니다.
+
+    // 주어진 좌표가 축소 비율을 적용합니다.
+    return {
+      id: point.id,
+      x: point.x * scale,
+      y: point.y * scale,
+    };
+  };
+
+  useEffect(() => {
+    if (points.length === 0) return;
+    const scaledPoints = points.map((point) => scale(point, points));
+    const ReverseYPoints = scaledPoints.map((point) => ({
+      id: point.id,
+      x: point.x,
+      y: 300 - point.y,
+    }));
+    setScaledPoints(ReverseYPoints);
+  }, [points]);
+
   return (
-    <Stage width={280} height={280} style={{display:"flex", justifyContent:"center", alignItems:"center", position:"relative"}}>
-      <Layer draggable of>
-        {drawShape()}
-      </Layer>
-    </Stage>
+    <div key={"div_Graph"}>
+      <svg width="300" height="300" viewBox="0 0 300 300">
+        <polygon
+          points={pointsToString(scaledPoints)}
+          fill="skyblue"
+          stroke="black"
+          strokeWidth="1"
+        />
+        {scaledPoints.length !== 0 &&
+          points.length === scaledPoints.length &&
+          scaledPoints.map((point, index) => {
+            console.log(point);
+            const dxMargin = point.x >= 290 && point.y <= 10 ? "-80px" : "-72px";
+            return (
+              <text
+                key={`${index}_${point.id}`}
+                x={point.x + 5}
+                y={point.y + 5}
+                fontSize="15"
+                fill="black"
+                textAnchor="large"
+                dx={point.x >= 290 ? dxMargin : ".2em"}
+                dy={point.y >= 290 ? "-15px" : "1em"}
+              >
+                {`${points[index].id}(${points[index].x},${points[index].y})`}
+              </text>
+            );
+          })}
+      </svg>
+    </div>
   );
 };
+
+// 사용 예시
+export default function PreViewShape(props) {
+  const { rowdata } = props;
+
+  // 삼각형의 좌표 예시입니다. 추가 좌표를 받아 다각형을 그릴 수 있습니다.
+  const points = [
+    { id: 1, x: 10, y: 10 },
+    { id: 2, x: 600, y: 10 },
+    { id: 3, x: 250, y: 800 },
+    { id: 4, x: 10, y: 250 },
+  ];
+
+  return <div>{rowdata && <Graph key={"Graph"} points={rowdata} />}</div>;
+}
